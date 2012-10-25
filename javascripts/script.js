1de72f96f1a3ad01acdb9664e72a9f49a300bdb6
Michael Porath <michael@porath.ch>
2012-10-25 00:37:47 -0700

    Github pages with index of all maps

---
+++ javascripts/script.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+script.py
+
+Created by Michael Porath on 2012-10-24.
+Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import getopt
+
+
+help_message = '''
+The help message goes here.
+'''
+
+
+class Usage(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    try:
+        try:
+            opts, args = getopt.getopt(argv[1:], "ho:v", ["help", "output="])
+        except getopt.error, msg:
+            raise Usage(msg)
+    
+        # option processing
+        for option, value in opts:
+            if option == "-v":
+                verbose = True
+            if option in ("-h", "--help"):
+                raise Usage(help_message)
+            if option in ("-o", "--output"):
+                output = value
+    
+    except Usage, err:
+        print >> sys.stderr, sys.argv[0].split("/")[-1] + ": " + str(err.msg)
+        print >> sys.stderr, "\t for help use --help"
+        return 2
+
+
+if __name__ == "__main__":
+    sys.exit(main())
